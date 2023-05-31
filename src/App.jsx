3ca14a6cadfcbfbee058b71a912828e3949dcf67
Hakim Firman <hakimastina@gmail.com>
2023-05-31 22:45:58 +0700

    fixed hamburger menu on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
-import Artikel from "./component/Artikel/Artikel";
+// import Artikel from "./component/Artikel/Artikel";
 import Footer from "./component/Footer/Footer";
 import HomePage from "./component/HomePage/HomePage";
+
 import Navbar from "./component/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarOffCanvas from "./component/Navbar/NavbarOffCanvas";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+      <NavbarOffCanvas/>
+    {/* <Navbar/> */}
         <Routes>
           {/* Isi route nya ya kyk page Homepage, search, aksi, article...contoh  */}
           <Route path="/" element={<HomePage />} />
