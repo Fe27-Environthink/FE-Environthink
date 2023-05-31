ddf0f9a2bcbd1f5079287d2e31ae51df8f109507
faraaryaresta <faraaryaresta@gmail.com>
2023-05-31 19:59:35 +0700

    navbar folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./component/Navbar";
+import Navbar from "./component/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
