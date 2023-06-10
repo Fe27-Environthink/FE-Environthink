a8d0304ee903bad773f756b3443f91056baab55d
faraaryaresta <faraaryaresta@gmail.com>
2023-06-10 23:30:53 +0700

    modified infografis detail

--- src/component/Infografis/Infografis.jsx
+++ src/component/Infografis/Infografis.jsx
@@ -32,7 +32,6 @@ function Infografis() {
             <div className="infografis" id="infografis">
                 <h3 className="text-start mb-3">Infografis</h3>
                 <div style={{border: "0.5px solid #bfbfbf"}}></div>
-
                 <div className="row pt-4" id="infografisContent">
                     {isLoading?(
                         <div className="text-center  d-flex justify-content-center align-items-center my-5 py-5">
@@ -41,31 +40,30 @@ function Infografis() {
                         <Spinner animation="border" variant="dark" />
                         </div>
                     ): filterData.map((item) => (
-                            <div key={item.id} className="col-md-6 col-lg-4 mb-3 pb-4">
-                                <div className="card card-infografis h-100">
-                                    <img src={item.images} style={{cursor: "pointer"}} className="card-img-top" data-bs-toggle="modal" data-bs-target="#${data[i].id}Backdrop" alt="artikel"/>
-                                    <div className="card-body">
-                                        <h1 className="card-title fs-6 title-infografis" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                            {item.titleInfografis}
-                                        </h1>
-                                        <div className="modal fade" id="${data[i].id}Backdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                            <div className="modal-dialog">
-                                            <div className="modal-content">
-                                                <div className="modal-header">
-                                                    <h1 className="modal-title fs-5" id="staticBackdropLabel">sfdgfh</h1>
-                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div className="modal-body">
-                                                    <img src="" className="card-img-top" alt="infografis"/>
-                                                </div>
+                        <div key={item.id} className="col-md-6 col-lg-4 mb-3 pb-4">
+                            <div className="card card-infografis h-100">
+                                <img src={item.images} style={{cursor: "pointer"}} className="card-img-top" data-bs-toggle="modal" data-bs-target="#${data[i].id}Backdrop" alt="infografis"/>
+                                <div className="card-body">
+                                    <h1 className="card-title fs-6 title-infografis" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                        {item.titleInfografis}
+                                    </h1>
+                                    <div className="modal fade" id="${item.id}Backdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div className="modal-dialog">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h1 className="modal-title fs-5" id="staticBackdropLabel">{item.titleInfografis}</h1>
+                                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
+                                            <div className="modal-body">
+                                                <img src={item.images} className="card-img-top" alt="infografis"/>
                                             </div>
+                                        </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                    
+                        </div>
+                    ))}
                 </div>
             </div>
             {!isLoading&&
