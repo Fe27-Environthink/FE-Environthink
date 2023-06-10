ef339d1d1fdd65b7ada9124d0b78919b801c65c2
Shou9.123.Myko <mikofernando2003@gmail.com>
2023-06-11 04:11:17 +0700

    hehehe

--- src/component/Infografis/Infografis.jsx
+++ src/component/Infografis/Infografis.jsx
@@ -3,8 +3,19 @@ import "./Infografis.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getInfografis } from "../../Redux/Action/infografisAction";
 import Spinner from "react-bootstrap/Spinner";
+import { Modal, Button } from "react-bootstrap";
 
 function Infografis() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   const dispatch = useDispatch();
   const { infografis, isLoading } = useSelector(
     (state) => state.infografisReducer
@@ -48,55 +59,36 @@ function Infografis() {
                       src={item.images}
                       style={{ cursor: "pointer" }}
                       className="card-img-top"
-                      data-bs-toggle="modal"
-                      data-bs-target={`#${item.id}Backdrop`}
+                      onClick={handleImageClick}
                       alt="infografis"
                     />
                     <div className="card-body">
                       <h1
                         className="card-title fs-6 title-infografis"
-                        data-bs-toggle="modal"
-                        data-bs-target="#staticBackdrop"
+                        onClick={handleImageClick}
                       >
                         {item.titleInfografis}
                       </h1>
-                      <div
-                        className="modal fade"
-                        id={`${item.id}Backdrop`}
-                        data-bs-backdrop="static"
-                        data-bs-keyboard="false"
-                        tabIndex="-1"
-                        aria-labelledby="staticBackdropLabel"
-                        aria-hidden="true"
-                      >
-                        <div className="modal-dialog">
-                          <div className="modal-content">
-                            <div className="modal-header">
-                              <h1
-                                className="modal-title fs-5"
-                                id="staticBackdropLabel"
-                              >
-                                {item.titleInfografis}
-                              </h1>
-                              <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                              ></button>
-                            </div>
-                            <div className="modal-body">
-                              <img
-                                src={item.images}
-                                className="card-img-top"
-                                alt="infografis"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
                     </div>
                   </div>
+
+                  <Modal show={showModal} onHide={handleCloseModal}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>{item.titleInfografis}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <img
+                        src={item.images}
+                        className="card-img-top"
+                        alt="infografis"
+                      />
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
                 </div>
               ))
             )}
