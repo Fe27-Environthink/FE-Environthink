803b4ef30235dd075c92d6aa39d99fa07e6d933c
Hakim Firman <hakimastina@gmail.com>
2023-06-13 02:56:33 +0700

    Update DetailAksi.jsx
    added aksi lainnya

--- src/component/Aksi/DetailAksi.jsx
+++ src/component/Aksi/DetailAksi.jsx
@@ -1,16 +1,19 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./Aksi.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
-import { getDetail,  } from "../../Redux/Action/AksiAction";
+import { getDataAksi, getDetail } from "../../Redux/Action/AksiAction";
 
 import FormPetisi from "./FormPetisi";
+import { FaUsers } from "react-icons/fa";
 
 function DetailAksi() {
   const { key } = useParams();
   const dispatch = useDispatch();
-  const { detailAksi, isLoading } = useSelector(
+  const [aksiLainnya, setAkasiLainnya] = useState([]);
+  const navigate = useNavigate();
+  const { detailAksi, listAksi, isLoading } = useSelector(
     (state) => state.AksiReducer
   );
 
@@ -20,7 +23,7 @@ function DetailAksi() {
       if (detailAksi.numberOfSupport == 0) {
         return (
           <h5 className="fw-bold" id="heading-form">
-            Belum ada dukungan 
+            Belum ada dukungan
           </h5>
         );
       } else if (detailAksi.numberOfSupport < detailAksi.target) {
@@ -64,7 +67,15 @@ function DetailAksi() {
   };
   useEffect(() => {
     dispatch(getDetail(key));
+    dispatch(getDataAksi());
   }, []);
+
+  useEffect(() => {
+    setAkasiLainnya(
+      listAksi.filter((item) => item.title != detailAksi.title).slice(0, 3)
+    );
+  }, [listAksi]);
+
   return (
     <>
       {isLoading ? (
@@ -158,13 +169,82 @@ function DetailAksi() {
                   </Link>
                 ) : (
                   <FormPetisi />
-                  
                 )}
               </div>
             </div>
           </div>
         </div>
       )}
+      <div className="container mt-4 mb-0">
+        <div className="aksi">
+          <h3 className="text-start ">Aksi Lainnya </h3>
+
+          <div style={{ border: "0.5px solid #bfbfbf" }} className="mb-0"></div>
+
+          <div className="row pt-2">
+            {isLoading ? (
+              <div className="text-center  d-flex justify-content-center align-items-center my-5 py-5">
+                <span className="mx-2 h1">loading</span>
+                <Spinner animation="border" variant="dark" />
+              </div>
+            ) : (
+              aksiLainnya.map((item) => (
+                <div key={item.id} className="aksiContent">
+                  <div className="row ms-1 me-1 mt-5 mb-5">
+                    <div className="col-md-4 p-0 me-4">
+                      <img id="aksiImage" src={item.image} alt="Images " />
+                    </div>
+                    <div className="col-md-7 ps-0 pe-0 mt-2">
+                      <p className="hashTag  m-0 p-0">
+                        <Link to={`/aksi/`} style={{ textDecoration: "none" }}>
+                          <span id="cathegory">Petisi</span>{" "}
+                          <span id="dot"> </span>
+                        </Link>
+                        {item != 0 &&
+                          item.hashtag.map((item) => (
+                            <Link
+                              to={`/aksi/terkait/${item}`}
+                              style={{ textDecoration: "none" }}
+                            >
+                              <span id="hashTag" className="p-2 hashTag ">
+                                # {item}
+                              </span>
+                            </Link>
+                          ))}
+                      </p>
+                      <a
+                        className="wrapperLinkTitleAksi"
+                        onClick={() => navigate(`/aksi/${item.id}`)}
+                      >
+                        <h3 className="titleAksi">{item.title}</h3>
+                      </a>
+                      <p className="descAksi text-dark wrapText">
+                        {item.paragraf1}
+                      </p>
+                      <p className="card-text  kontributorAksi sub-title d-flex align-items-center gap-2">
+                        <FaUsers />
+                        {item.numberOfSupport == 0 ? (
+                          <span className="fw-medium fs-6">
+                            Belum ada dukungan
+                          </span>
+                        ) : item.numberOfSupport < item.target ? (
+                          <span className="fw-medium fs-6">
+                            {item.numberOfSupport} orang mendukung
+                          </span>
+                        ) : (
+                          <span className="fw-medium fs-6">
+                            Petisi Mencapai Kemenangan
+                          </span>
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
