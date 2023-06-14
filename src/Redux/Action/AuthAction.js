93bf61cb6f585d66e66b8a8eda50502e3270aa53
Hakim Firman <hakimastina@gmail.com>
2023-06-14 23:35:36 +0700

    add register

--- src/Redux/Action/AuthAction.js
+++ src/Redux/Action/AuthAction.js
@@ -1,41 +1,55 @@
-import axios from "axios"
-import Swal from "sweetalert2"
+import axios from "axios";
+import Swal from "sweetalert2";
 
-export const AUTH ="AUTH"
-export const GET_DATA ="GET_DATA"
+export const AUTH = "AUTH";
+export const REGISTER = "REGISTER";
+export const GET_DATA = "GET_DATA";
 
-const auth =(payload)=>{
-    
-    return{
-        type:AUTH,
-        payload
+const auth = (payload) => {
+  return {
+    type: AUTH,
+    payload,
+  };
+};
+const register = (payload) => {
+  return {
+    type: REGISTER,
+    payload,
+  };
+};
 
-    }
-}
-
-
-export const sigInUser=(data)=>async(dispatch)=>{
-    console.log("data",data);
-    const json = JSON.stringify(data);
-    console.log("convert json",json);
-    const url='https://api-fe27be9-environthink.cyclic.app/user/signin/'
-
+export const sigInUser = (data) => async (dispatch) => {
+  console.log("data", data);
+  const json = JSON.stringify(data);
+  console.log("convert json", json);
+  const url = "https://api-fe27be9-environthink.cyclic.app/user/signin/";
 
   try {
-   const res =await axios.post(url,data);
+    const res = await axios.post(url, data);
     console.log(res);
-    Swal.fire("Sukses", "Login Success", "success").then(()=> dispatch(auth(res.data)));
-   
+    Swal.fire("Sukses", "Login Success", "success").then(() =>
+      dispatch(auth(res.data))
+    );
   } catch (error) {
     console.log(error.response.data);
-    Swal.fire(
-        "Login Failed",
-        `${error.response.data.message}`,
-        "error"
-      );
-
+    Swal.fire("Login Failed", `${error.response.data.message}`, "error");
   }
-// const res = await axios('https://api-fe27be9-environthink.cyclic.app/user')
-// 
+};
 
-}
+export const sigUpUser = (data) => async (dispatch) => {
+  console.log("data", data);
+  const json = JSON.stringify(data);
+  console.log("convert json", json);
+  const url = "https://api-fe27be9-environthink.cyclic.app/user/signup";
+
+  try {
+    const res = await axios.post(url, data);
+    console.log(res);
+    Swal.fire("Sukses", "Login Success", "success").then(() =>
+      dispatch(register(res.data))
+    );
+  } catch (error) {
+    console.log(error.response.data);
+    // Swal.fire("Login Failed", `${error.response.data.message}`, "error");
+  }
+};
--- src/component/Auth/Register.jsx
+++ src/component/Auth/Register.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbars from '../Navbar/Navbars'
+import { useDispatch } from 'react-redux';
+import { sigUpUser } from '../../Redux/Action/AuthAction';
 
 function Register() {
+  const dispatch = useDispatch()
+  const [inputRegister,setInputRegister] = useState({
+    username: "",
+    password: "",
+    email: "",
+    telepon: "",
+    kota: "",
+    role: "",
+  })
+
+  const handleChangeRegister = (e) => {
+    setInputRegister({
+      ...inputRegister,
+      [e.target.name]: e.target.value,
+    });
+    console.log(inputRegister);
+  };
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    dispatch(sigUpUser(inputRegister));
+  };
   return (
     <>
      <Navbars />
@@ -16,26 +40,26 @@ function Register() {
             <div className="col-lg-6">
               <div className="p-5">
                 <h1>Daftarkan Akun</h1>
-                <form id="register-form">
+                <form id="register-form" onSubmit={handleRegister}>
                   <div className="form-group">
                     <label htmlFor="username" className="mb-2 fw-bold ">Username:</label>
-                    <input type="text" id="username" name="username" className="form-control" required />
+                    <input type="text" id="username"   value={inputRegister.username} onChange={handleChangeRegister} name="username" className="form-control" required />
                   </div>
                   <div className="form-group">
                     <label htmlFor="email" className="mb-2 fw-bold ">Emaill:</label>
-                    <input type="email" id="email" name="email" className="form-control" required />
+                    <input type="email" id="email" name="email" value={inputRegister.email} onChange={handleChangeRegister} className="form-control" required />
                   </div>
                   <div className="form-group">
                     <label htmlFor="kota" className="mb-2 fw-bold ">Kota:</label>
-                    <input type="text" id="kota" name="kota" className="form-control" required />
+                    <input type="text" id="kota" name="kota" value={inputRegister.kota} onChange={handleChangeRegister} className="form-control" required />
                   </div>
                   <div className="form-group">
                     <label htmlFor="telepon" className="mb-2 fw-bold ">telepon:</label>
-                    <input type="text" id="telepon" name="telepon" className="form-control" required />
+                    <input type="text" id="telepon" name="telepon" value={inputRegister.telepon} onChange={handleChangeRegister} className="form-control" required />
                   </div>
                   <div className="form-group mb-3">
                     <label htmlFor="password" className="mb-2 fw-bold ">Password:</label>
-                    <input type="password" id="password" name="password" className="form-control" required />
+                    <input type="password" id="password" name="password" value={inputRegister.password} onChange={handleChangeRegister} className="form-control" required />
                   </div>
                   <button type="submit " className="btn btn-main  w-100">Buat Akun</button>
                 </form>      
